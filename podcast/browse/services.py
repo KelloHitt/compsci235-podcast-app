33a44dac9955bcead6db167d2e371b23571af8ad
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-08-15 22:51:49 +1200

    update the browsing logic that can also browsing by category

--- podcast/browse/services.py
+++ podcast/browse/services.py
@@ -22,3 +22,24 @@ def get_podcasts_by_page(repository: AbstractRepository, page_number: int):
         'current_page': page_number,
         'last_page': last_page
     }
+
+
+def get_podcasts_by_category(repository: AbstractRepository, category: str, page_number: int):
+    podcast_ids = repository.get_podcasts_ids_for_category(category)
+    list_of_podcasts = repository.get_podcasts_by_id(podcast_ids[(page_number - 1) * 10: page_number * 10])
+
+    page_size = 10
+    has_next = (page_number * page_size) < len(podcast_ids)
+    has_previous = page_number > 1
+    last_page = (len(podcast_ids) + page_size - 1) // page_size
+
+    return {
+        'podcasts': list_of_podcasts,
+        'has_next': has_next,
+        'has_previous': has_previous,
+        'next_page': page_number + 1 if has_next else page_number,
+        'previous_page': page_number - 1 if has_previous else page_number,
+        'current_page': page_number,
+        'last_page': last_page,
+        'category': category
+    }
