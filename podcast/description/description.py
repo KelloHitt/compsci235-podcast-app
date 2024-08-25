66b7287ab8966707ffde5ff5cf33986d006e86b8
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-08-25 12:19:08 +1200

    add checking to ensure podcast_id is valid

--- podcast/description/description.py
+++ podcast/description/description.py
@@ -11,6 +11,12 @@ description_blueprint = Blueprint('description_bp', __name__)
 def show_description():
     podcast_id = request.args.get('podcast_id', default=1, type=int)
 
+    # Ensure podcast_id is within valid range
+    if podcast_id > 1000:
+        podcast_id = 1000
+    if podcast_id < 1:
+        podcast_id = 1
+
     podcast = services.get_podcast_by_id(repository.repo_instance, podcast_id)
     episodes = sorted(podcast.episodes, key=lambda episode: episode.date)
     categories = utilities.get_categories()['categories']
