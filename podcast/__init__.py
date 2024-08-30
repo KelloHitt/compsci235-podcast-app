c3c854ad42f93bf663d3e3428707abd0a62963b9
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-08-30 13:01:09 +1200

    update initialization with config

--- podcast/__init__.py
+++ podcast/__init__.py
@@ -1,22 +1,32 @@
 """Initialize Flask app."""
 import os
 from pathlib import Path
-from flask import Flask, render_template
+
+from flask import Flask
+
 import podcast.adapters.repository as repo
-from podcast.adapters.memory_repository import MemoryRepository, populate_data
 from podcast.adapters.datareader.csvdatareader import CSVDataReader
+from podcast.adapters.memory_repository import MemoryRepository, populate_data
 
 
-def create_app():
+def create_app(test_config=None):
     """Construct the core application."""
 
     # Create the Flask app object.
     app = Flask(__name__)
 
+    # Configure the app from configuration-file settings.
+    app.config.from_object('config.Config')
+    data_path = Path(os.path.join(os.path.dirname(os.path.abspath(__file__)), "adapters/data"))
+
+    if test_config is not None:
+        # Load test configuration, and override any configuration settings.
+        app.config.from_mapping(test_config)
+        data_path = app.config['TEST_DATA_PATH']
+
     # Create the MemoryRepository implementation for a memory-based repository.
     repo.repo_instance = MemoryRepository()
     # Fill the content with the repository from the provided csv files
-    data_path = Path(os.path.join(os.path.dirname(os.path.abspath(__file__)), "adapters/data"))
     populate_data(repo.repo_instance, data_path)
 
     with app.app_context():
