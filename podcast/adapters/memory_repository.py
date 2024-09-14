faff664f6a4da432fe0b4fa8160faee5ed1b6c8a
owak638 <owak638@aucklanduni.ac.nz>
2024-09-14 15:17:05 +1200

    populating reviews from csv reader

--- podcast/adapters/memory_repository.py
+++ podcast/adapters/memory_repository.py
@@ -95,11 +95,13 @@ def populate_data(repo: AbstractRepository, data_path: Path):
     reader = CSVDataReader()
     reader.load_podcasts_authors_categories(data_path)
     reader.load_episodes(data_path)
+    reader.load_reviews(data_path)
 
     podcasts = reader.dataset_of_podcasts
     authors = reader.dataset_of_authors
     categories = reader.dataset_of_categories
     episodes = reader.dataset_of_episodes
+    reviews = reader.dataset_of_reviews
 
     for author in authors.values():
         repo.add_author(author)
@@ -112,6 +114,9 @@ def populate_data(repo: AbstractRepository, data_path: Path):
 
     for episode in episodes:
         repo.add_episode(episode)
+
+    for review in reviews:
+        repo.add_review(review.content, review.rating, review.podcast, review.reviewer)
 
 def add_review(self, description: str, rating: int, podcast: Podcast, user: User):
     review = Review(len(self.__reviews) + 1, podcast, user, rating, description)
