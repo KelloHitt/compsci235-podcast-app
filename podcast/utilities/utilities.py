13caedcf8bb597162a731c22c7e783f70c04958e
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-09-15 12:39:43 +1200

    create a method to get username in the session

--- podcast/utilities/utilities.py
+++ podcast/utilities/utilities.py
@@ -1,3 +1,5 @@
+from flask import session
+
 import podcast.adapters.repository as repository
 import podcast.utilities.services as services
 
@@ -5,3 +7,10 @@ import podcast.utilities.services as services
 def get_categories():
     categories = services.get_categories(repository.repo_instance)
     return {'categories': categories}
+
+
+def get_username():
+    username = None
+    if "username" in session:
+        username = session["username"]
+    return username
