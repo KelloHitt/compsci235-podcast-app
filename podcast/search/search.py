512d32c3b8da7a8b4ae6bce38b287db74370c610
Venus <vcha997@aucklanduni.ac.nz>
2024-09-16 12:11:07 +1200

    Created blueprint for search function

--- podcast/search/search.py
+++ podcast/search/search.py
@@ -3,39 +3,28 @@ import podcast.adapters.repository as repository
 import podcast.search.services as services
 import podcast.utilities.utilities as utilities
 
-
 search_blueprint = Blueprint(
     'search_bp', __name__)
+
+
 @search_blueprint.route('/search', methods=['GET'])
 def search():
     return render_template('search.html')
-
 
 
 @search_blueprint.route('/results', methods=['GET'])
 def results():
     search_query = request.args.get('q', '')
     search_field = request.args.get('field', 'title')
-    podcasts = services.get_podcasts_list(repository)
 
-
-    # Filter podcasts based on the search criteria
-    results = []
-    if (search_field == 'category'):
-        for podcast in podcasts:
-            if (search_field in podcast.categories):
-                results.append(podcast)
-    elif (search_field == 'title'):
-        for podcast in podcasts:
-            if (search_field in podcast.title):
-                results.append(podcast)
+    podcasts = []
+    if search_field == 'category':
+        podcasts = services.get_podcasts_category(repository.repo_instance, search_query)
+    elif search_field == 'title':
+        podcasts = services.get_podcasts_title(repository.repo_instance, search_query)
     else:
-        for podcast in podcasts:
-            if (search_field in podcast.author):
-                results.append(podcast)
+        podcasts = services.get_podcasts_author(repository.repo_instance, search_query)
 
+    return render_template('search.html', results=podcasts, query=search_query, field=search_field)
 
-
-
-    return render_template('search.html', results=results, query=search_query, field=search_field)
 
