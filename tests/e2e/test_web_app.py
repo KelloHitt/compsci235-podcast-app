4577e68b3760617bb1a7de91e7e4cf14f00d0e32
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-09-20 23:16:38 +1200

    added test cases for login

--- tests/e2e/test_web_app.py
+++ tests/e2e/test_web_app.py
@@ -1,26 +1,58 @@
-# TODO: This file is for Integration testing
-
-
 import pytest
+from unittest.mock import patch
+
 from flask import session
 
 
 def test_register(client):
-    pass
+    response_code = client.get("/authentication/register").status_code
+    assert response_code == 200
+
+    response = client.post("/authentication/register", data={"username": "testUser1", "password": "testUserpassword1"})
+    assert response.headers["Location"] == "/authentication/login"
 
 
 @pytest.mark.parametrize(
-    ("username", "password", "message"),
+    ('username', 'password', 'message'),
     (
-        # TODO: set up testing parameters
-    ),
-)
-def test_register_with_invalid_input(client, username, password, message):
-    pass
+            ('', '', b'Your username is required'),
+            ('cj', '', b'Your username is too short'),
+            ('test', '', b'Your password is required'),
+            ('test', 'test', b'Your password must be at least 8 characters, and contain an upper case letter,\
+            a lower case letter and a digit'),
+            ("testUser1", "testUserpassword1", b"Your username is already taken - please try another one."),
+    ))
+def test_register_with_invalid_input(auth, client, username, password, message):
+    # Check that attempting to register with invalid combinations of user name and password generate appropriate error
+    # messages.
+    auth.register()  # register user with username "testUser1"
+    response = client.post(
+        '/authentication/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
 
 
 def test_login(client, auth):
-    pass
+    # Mock the call to get_random_podcasts_info to avoid sampling errors
+    with patch('podcast.home.services.get_random_podcasts_info') as mock_get_podcasts:
+        mock_get_podcasts.return_value = []  # Simulate an empty list of podcasts or provide mock data
+
+        # create user
+        response = auth.register()
+        # check if redirects to login page
+        assert response.headers["Location"] == "/authentication/login"
+        # Check that we can retrieve the login page
+        status_code = client.get('/authentication/login').status_code
+        assert status_code == 200
+        # test login
+        response = auth.login()
+        # test if successful and redirected to home page
+        assert response.headers["Location"] == "/"
+        # Check that a session has been created for the logged-in user.
+        with client:
+            client.get('/')
+            assert session['username'] == "testUser1"
 
 
 def test_logout(client, auth):
