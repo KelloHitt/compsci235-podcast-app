831eb0a81b0f0bbbfa4578e15012f3d635f55dd1
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-09-30 15:19:33 +1300

    Set up database_repository

---
+++ podcast/adapters/database_repository.py
@@ -0,0 +1,129 @@
+from typing import List
+
+from sqlalchemy.orm import scoped_session
+
+from podcast.adapters.repository import AbstractRepository
+from podcast.domainmodel.model import Review, Podcast, User, Playlist, Episode, Category, Author
+
+
+class SessionContextManager:
+    def __init__(self, session_factory):
+        self.__session_factory = session_factory
+        self.__session = scoped_session(self.__session_factory)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.rollback()
+
+    @property
+    def session(self):
+        return self.__session
+
+    def commit(self):
+        self.__session.commit()
+
+    def rollback(self):
+        self.__session.rollback()
+
+    def reset_session(self):
+        # this method can be used e.g. to allow Flask to start a new session for each http request,
+        # via the 'before_request' callback
+        self.close_current_session()
+        self.__session = scoped_session(self.__session_factory)
+
+    def close_current_session(self):
+        if not self.__session is None:
+            self.__session.close()
+
+
+# TODO: implement all the abstract methods with real codes in this class
+class SqlAlchemyRepository(AbstractRepository):
+
+    def __init__(self, session_factory):
+        self._session_cm = SessionContextManager(session_factory)
+
+    def close_session(self):
+        self._session_cm.close_current_session()
+
+    def reset_session(self):
+        self._session_cm.reset_session()
+
+    def add_podcast(self, podcast: Podcast):
+        pass
+
+    def get_podcast(self, podcast_id: int) -> Podcast:
+        pass
+
+    def get_podcasts_by_id(self, id_list: list) -> List[Podcast]:
+        pass
+
+    def get_podcasts_by_page(self, page: int, page_size: int) -> List[Podcast]:
+        pass
+
+    def get_number_of_podcasts(self) -> int:
+        pass
+
+    def get_podcasts_ids_for_category(self, category_name: str) -> List[int]:
+        pass
+
+    def has_next_page(self, current_page: int, page_size: int) -> bool:
+        pass
+
+    def has_previous_page(self, current_page: int) -> bool:
+        pass
+
+    def get_next_page(self, current_page: int, page_size: int) -> int:
+        pass
+
+    def get_previous_page(self, current_page: int) -> int:
+        pass
+
+    def get_categories(self) -> List[Category]:
+        pass
+
+    def add_episode(self, episode: Episode):
+        pass
+
+    def get_number_of_episodes(self) -> int:
+        pass
+
+    def get_episode(self, episode_id: int) -> Episode:
+        pass
+
+    def add_author(self, author: Author):
+        pass
+
+    def add_category(self, category: Category):
+        pass
+
+    def add_user(self, username: str, password: str):
+        pass
+
+    def get_user(self, username: str) -> User:
+        pass
+
+    def get_podcasts_by_category(self, category_query: str) -> list:
+        pass
+
+    def get_podcasts_by_title(self, title: str) -> list:
+        pass
+
+    def get_podcasts_by_author(self, author: str) -> list:
+        pass
+
+    def add_to_playlist(self, username: str, episode: Episode):
+        pass
+
+    def get_users_playlist(self, username: str) -> Playlist:
+        pass
+
+    def add_review(self, podcast: Podcast, user: User, rating: int, description: str):
+        pass
+
+    def get_users_reviews(self, username: str) -> List[Review]:
+        pass
+
+    def delete_review(self, review_id: int):
+        pass
