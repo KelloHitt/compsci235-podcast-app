60de50e6764f8b61b5db2e78be0b7ab7bb4dc175
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-10-09 22:20:16 +1300

    Add initial testing case for populate database

--- tests_db/unit/test_populate_database.py
+++ tests_db/unit/test_populate_database.py
@@ -1,1 +1,11 @@
-# TODO: testing cases
+from sqlalchemy import inspect
+from podcast.adapters.orm import mapper_registry
+
+
+def test_database_populate_inspect_table_names(database_engine):
+    # Get table information
+    inspector = inspect(database_engine)
+    assert inspector.get_table_names() == ['authors', 'categories', 'episodes', 'playlist_episodes', 'playlists',
+                                           'podcast_categories', 'podcasts', 'reviews', 'users']
+
+# TODO: more testing cases
