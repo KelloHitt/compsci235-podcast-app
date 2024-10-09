a89dc29be83fa039bf7fe5699a75b3b5787d4627
owak638 <owak638@aucklanduni.ac.nz>
2024-10-09 15:37:48 +1300

    orm testing cases

--- tests_db/unit/test_orm.py
+++ tests_db/unit/test_orm.py
@@ -1,4 +1,4 @@
-from podcast.domainmodel.model import User
+from podcast.domainmodel.model import User, Podcast, Episode, Author, Review, Category
 
 def insert_user(empty_session, values=None):
     new_username = "John"
@@ -55,3 +55,49 @@ def test_saving_of_users_with_common_username(empty_session):
         user = User("John", "123456Ab")
         empty_session.add(user)
         empty_session.commit()
+
+def insert_podcast(empty_session):
+    empty_session.execute('INSERT INTO podcasts (title, image_url, description, language, website_url, author_id, itunes_id) VALUES'
+                          '("D-Hour Radio Network",'
+                          '"http://is3.mzstatic.com/image/thumb/Music118/v4/b9/ed/86/b9ed8603-d94b-28c5-5f95-8b7061bf22fa/source/600x600bb.jpg", '
+                          '"The D-Hour Radio Network is the home of real entertainment radio and ""THE"" premiere online radio network. We showcase dynamically dynamite radio shows for the sole purpose of entertaining your listening ear. Here on the D-hour Show Radio network we take pride in providing an outlet for Celebrity Artists, Underground Artists, Indie Artists, Producers, Entertainers, Entrepreneurs, Internet Stars and future business owners. We discuss topics of all forms and have a great time while doing so. We play all your favorite hits in the forms of Celebrity, Indie, Hip Hop, Soul/R&B, Pop, and everything else you want and consider popular. If you would like yourself and or your music to be showcased on our radio network submit email requests for music airplay, interviews and etc.. to:  dhourshow@gmail.com and we will get back to you promptly. Here at the D-Hour Radio Network we are Family and all of our guests, listeners and loyal fans are family too.  So tune into the D-Hour Radio Network and join the Family! ", '
+                          '"English", '
+                          '"http://www.blogtalkradio.com/dhourshow", '
+                          '1, '
+                          '538283940,')
+    row = empty_session.execute('SELECT id from podcasts').fetchone()
+    return row[0]
+
+def make_podcast():
+    author = make_author()
+    podcast = Podcast(1, author, "Podcast 1", "image", "Description 1", "Website 1", 1234, "English")
+    return podcast
+,
+def insert_episode(empty_session):
+    pass
+
+def make_episode():
+    podcast = make_podcast()
+    episode = Episode(1, podcast, "Title", "Website", "Description", 123, "8/10/2024")
+
+def insert_author(empty_session):
+    pass
+def make_author():
+    author = Author(1, "Author 1")
+    return author
+
+def insert_review(empty_session):
+    pass
+
+def make_review():
+    podcast = make_podcast()
+    reviewer = make_user()
+    review = Review(1, podcast, reviewer, 1, "Review Text")
+    return review
+
+def insert_categories(empty_session):
+    empty_session.execute('INSERT INTO categories (category_name) VALUES ("Sports") ("News")')
+
+def make_category():
+    category = Category(1, "Sport")
+    return category
