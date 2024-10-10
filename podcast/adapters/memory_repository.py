0715a818d25716840601d340f187f4afa5ea9cd9
crystal941 <107521386+crystal941@users.noreply.github.com>
2024-10-10 19:06:58 +1300

    Delete debug codes

--- podcast/adapters/memory_repository.py
+++ podcast/adapters/memory_repository.py
@@ -33,7 +33,6 @@ class MemoryRepository(AbstractRepository):
         return self.__podcasts[start_index:end_index]
 
     def get_number_of_podcasts(self) -> int:
-        print("HIII from memory repository")
         return len(self.__podcasts)
 
     def get_podcasts_ids_for_category(self, category_name: str) -> List[int]:
